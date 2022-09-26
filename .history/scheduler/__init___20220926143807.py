2ce73ff8e34c1b8320c695bcdbde59134cfb7fcd
Shanshan <soryajiang7@gmail.com>
2022-09-26 15:46:04 +1000

    first commit

---
+++ .history/scheduler/__init___20220926143807.py
@@ -0,0 +1,72 @@
+try:
+    from flask import Flask
+    from celery import Celery
+    from datetime import timedelta
+except Exception as e:
+    print("Error : {} ".format(e))
+
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+
+app = Flask(__name__)
+app.config['CELERY_BACKEND'] = "redis://redis:6379/0"
+app.config['CELERY_BROKER_URL'] = "redis://redis:6379/0"
+
+app.config['CELERYBEAT_SCHEDULE'] = {   
+    'say-every-5-seconds': {
+        'task': 'fiveseconds',
+        'schedule': timedelta(seconds=5)
+    },
+    'say-every-10-seconds': {
+        'task': 'tenseconds',
+        'schedule': timedelta(seconds=10)
+    },
+    'say-every-day-evening': {
+        'task': 'twelve',
+        'schedule': crontab(hour=14, minute=0)
+    },
+    'say-every-day-evening': {
+        'task': 'one',
+        'schedule': crontab(hour=15, minute=0)
+    },
+    'say-every-monday-morning': {
+        'task': 'monday',
+        'schedule': crontab(hour=0, minute=30, day_of_week=7)
+    }
+}
+
+app.config['CELERY_TIMEZONE'] = 'UTC'
+celery_app = make_celery(app)
+
+@celery_app.task(name='fiveseconds')
+def fiveseconds():
+    print ('fiveseconds')
+
+@celery_app.task
+def tenseconds():
+    print ('tenseconds')
+
+@celery_app.task
+def twelve():
+    print ('twelve')
+
+@celery_app.task
+def one():
+    print ('one')
+
+@celery_app.task
+def monday():
+    print ('monday')
---
+++ .history/scheduler/__init___20220926154058.py
@@ -0,0 +1,48 @@
+try:
+    from flask import Flask
+    from celery import Celery
+    from datetime import timedelta
+    from celery.schedules import crontab
+except Exception as e:
+    print("Error : {} ".format(e))
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+
+app = Flask(__name__)
+app.config['CELERY_BACKEND'] = "redis://redis:6379/0"
+app.config['CELERY_BROKER_URL'] = "redis://redis:6379/0"
+app.config['CELERY_TIMEZONE'] = 'UTC'
+celery_app = make_celery(app)
+
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(5.0, test.s('five'), name='add every 5')
+
+    sender.add_periodic_task(10.0, test.s('ten'), name='add every 10')
+
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        test.s('Happy Mondays!'),
+    )
+
+@celery_app.task
+def test(arg):
+    print(arg)
+
+@app.task
+def add(x, y):
+    z = x + y
+    print(z)
